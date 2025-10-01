e3e2a750a75771e1c703d94f9a697a87b60233fc
citrarpl2 <toraltte.wavs@gmail.com>
2025-10-01 10:49:02 +0700

    tugas 1

--- jobsheet5/ifCetakKRS05.java
+++ jobsheet5/ifCetakKRS05.java
@@ -9,13 +9,15 @@ public class ifCetakKRS05 {
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // }
         
-        else {
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.");
-        }
+        // else {
+        //     System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.");
+        // } 
+        String ukt = (uktLunas) ? "Pembayaran UKT terverifikasi, Silahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println(ukt);
     }   
 }
